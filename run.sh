bc54528ec942d98893deec7944ea243c31de9503
kimu05 <kimu.000000218@gmail.com>
2023-07-17 04:35:15 +0900

    run.sh修正

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/.local/bin/streamlit run /home/ec2-user/my-app/app.py  # systemdから実行するため、streamlitの実行ファイルはフルパスで指定
+/usr/local/bin/streamlit run /home/ec2-user/my-app/app.py  # systemdから実行するため、streamlitの実行ファイルはフルパスで指定
